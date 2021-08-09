867d2ee19c3bcfcd115c009f9021dee4aa44ed25
taketototo <s246153@wakayama-u.ac.jp>
2021-08-09 18:40:40 +0900

    カラーフィルタ画面からスタート画面への画面遷移

--- app/src/main/java/com/example/clearschedule/ColorfilterActivity.java
+++ app/src/main/java/com/example/clearschedule/ColorfilterActivity.java
@@ -1,6 +1,9 @@
 package com.example.clearschedule;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,13 @@ public class ColorfilterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colorfilter);
+
+        final Button backbutton = findViewById(R.id.backbutton);
+        backbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ColorfilterActivity.this,StartscreenActivity.class);
+                startActivity(intent);
+            }
+        }); /*カラーフィルタ画面からスタート画面への画面遷移*/
     }
 }
