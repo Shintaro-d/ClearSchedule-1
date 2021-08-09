9980721b2edc42b047e5b6fc5fe549ffea615e91
taketototo <s246153@wakayama-u.ac.jp>
2021-08-09 22:40:22 +0900

    ScheduleData.javaにラベル情報を追加した。 ScheduleData.javaのidとdateの型を変更した。

--- app/src/main/java/com/example/clearschedule/ScheduleData.java
+++ app/src/main/java/com/example/clearschedule/ScheduleData.java
@@ -3,16 +3,19 @@ package com.example.clearschedule;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.util.Date;
+
 /**
  * 予定データクラス（Room）
  */
 @Entity(tableName = "account_data") //テーブル名を定義
 public class ScheduleData {
     @PrimaryKey(autoGenerate = true)
-    public int id;           //「id」カラムを定義
+    public long id;           //「id」カラムを定義
     public String title;   //「タイトル」カラムを定義
     public int color;        //「色// 」カラムを定義
-    public long date;        //「日付」カラムを定義
+    public Date date;        //「日付」カラムを定義
+    public int label; //「ラベル」カラムを定義
 
     /**
      * コンストラクタ
@@ -20,17 +23,18 @@ public class ScheduleData {
      * @param color   色
      * @param date    日付
      */
-    public ScheduleData(String title, int color, long date) {
+    public ScheduleData(String title, int color, Date date, int label) {
         this.title = title;  //「タイトル」を設定
         this.color = color;      //「色」を設定
         this.date = date;        //「日付」を設定
+        this.label = label; //「ラベル」を設定
     }
 
     /**
      * 「id」を取得（Getter）
      * @return id
      */
-    public int getId() {
+    public long getId() {
         return id;
     }
 
@@ -54,8 +58,17 @@ public class ScheduleData {
      *　「日付」を取得（Getter）
      * @return
      */
-    public long getDate() {
+    public Date getDate() {
         return date;
+    }
+
+    /**
+     *  「ラベル」を取得（Getter）
+     * @return
+     */
+
+    public int getLabel() {
+        return label;
     }
 
     /**
@@ -63,12 +76,15 @@ public class ScheduleData {
      * @param title 更新する「タイトル」
      * @param color   更新する「色」
      * @param date    更新する「日付」
+     * @param label 更新する「ラベル」
      * @return 更新した「予定データ」
      */
-    public ScheduleData update(String title, int color, long date){
+    public ScheduleData update(String title, int color, Date date, int label){
         this.title = title;  //「タイトル」を設定
         this.color = color;      //「色」を設定
         this.date = date;        //「日付」を設定
+        this.label = label; //「ラベル」を設定
         return this;
     }
-}
+} /*idの型をlong型に、dateの型をDate型にした。
+　　問題が出てくれば元に戻す*/
